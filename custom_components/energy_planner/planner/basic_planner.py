71f0a8534e89543c6a74e4af4c5d501f769de3ff
Jonathan Ek <jonathan@paaventyr.se>
2025-03-06 20:13:58 +0100

    try fetching yesterdays data from nordpool

--- custom_components/energy_planner/planner/basic_planner.py
+++ custom_components/energy_planner/planner/basic_planner.py
@@ -3,7 +3,7 @@ import logging
 
 from homeassistant.core import ServiceCall, HomeAssistant
 
-from .nordpool_utils import join_result_for_correct_time
+from .nordpool_utils import join_result_for_correct_time, parse_json
 from ..const import DATE_TIME_ENTITIES, DOMAIN, TIME_ENTITIES, SELECT_ENTITIES, SWITCH_ENTITIES, SENSOR_ENTITIES, NUMBER_ENTITIES
 
 _LOGGER = logging.getLogger(__name__)
@@ -28,8 +28,8 @@ async def reset(hass: HomeAssistant):
 
 async def planner(hass: HomeAssistant, call: ServiceCall):
     nordpool_entity_id = hass.data[DOMAIN]['config'].get("nordpool_entity_id")
-    nordpool_currency = nordpool_entity_id.split('_')[3]
-    nordpool_area = nordpool_entity_id.split('_')[2]
+    nordpool_currency = str(nordpool_entity_id.split('_')[3]).upper()
+    nordpool_area = str(nordpool_entity_id.split('_')[2]).upper()
 
     nordpool_state = hass.states.get(nordpool_entity_id)
     if nordpool_state is None:
@@ -45,23 +45,26 @@ async def planner(hass: HomeAssistant, call: ServiceCall):
     tomorrow_valid = attributes.get('tomorrow_valid')
     if not tomorrow_valid:
         raise ValueError("Tomorrow's prices are not valid")
-
     yesterdays_values = await hass.services.async_call('nordpool', 'hourly', {
-        "currency": str(nordpool_currency).upper(),
-        "area": str(nordpool_area).upper(),
+        "currency": nordpool_currency,
+        "area": nordpool_area,
         "date": (dt.datetime.now() - dt.timedelta(days=1)).strftime("%Y-%m-%d")
     }, True, return_response=True)
     todays_values = await hass.services.async_call('nordpool', 'hourly', {
-        "currency": str(nordpool_currency).upper(),
-        "area": str(nordpool_area).upper(),
+        "currency": nordpool_currency,
+        "area": nordpool_area,
         "date": (dt.datetime.now()).strftime("%Y-%m-%d")
     }, True, return_response=True)
     tomorrows_values = await hass.services.async_call('nordpool', 'hourly', {
-        "currency": str(nordpool_currency).upper(),
-        "area": str(nordpool_area).upper(),
+        "currency": nordpool_currency,
+        "area": nordpool_area,
         "date": (dt.datetime.now() + dt.timedelta(days=1)).strftime("%Y-%m-%d")
     }, True, return_response=True)
-    res = await join_result_for_correct_time([yesterdays_values, todays_values, tomorrows_values], dt.datetime.now())
+    res = await join_result_for_correct_time([
+        parse_json(yesterdays_values, nordpool_currency, areas=[nordpool_area]),
+        parse_json(todays_values, nordpool_currency, areas=[nordpool_area]),
+        parse_json(tomorrows_values, nordpool_currency, areas=[nordpool_area])
+    ], dt.datetime.now())
     _LOGGER.info("Nordpool response: %s", res)
 
     yesterday = [(i, x['entryPerArea'][str(nordpool_area).upper()], -1) for i, x in enumerate(res['multiAreaEntries'])]
--- custom_components/energy_planner/planner/nordpool_utils.py
+++ custom_components/energy_planner/planner/nordpool_utils.py
@@ -1,9 +1,11 @@
+# Heavily based on https://github.com/custom-components/nordpool/blob/master/custom_components/nordpool/aio_price.py
 import logging
 from collections import defaultdict
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from datetime import timezone as ts
-
+from dateutil.parser import parse as parse_dt
+from pytz import timezone, utc
 from homeassistant.util import dt as dt_utils
 
 _LOGGER = logging.getLogger(__name__)
@@ -32,13 +34,104 @@ tzs = {
     "GER": "Europe/Berlin",
 }
 
+
+def _parse_dt(time_str):
+    """Parse datetimes to UTC from Stockholm time, which Nord Pool uses."""
+    time = parse_dt(time_str, tzinfos={"Z": timezone("Europe/Stockholm")})
+    if time.tzinfo is None:
+        return timezone("Europe/Stockholm").localize(time).astimezone(utc)
+    return time.astimezone(utc)
+
+def _conv_to_float(s):
+    """Convert numbers to float. Return infinity, if conversion fails."""
+    # Skip if already float
+    if isinstance(s, float):
+        return s
+    try:
+        return float(s.replace(",", ".").replace(" ", ""))
+    except ValueError:
+        return float("inf")
+
+def parse_json(data, currency=None, areas=None):
+    """
+    Parse json response from fetcher.
+    Returns dictionary with
+        - start time
+        - end time
+        - update time
+        - currency
+        - dictionary of areas, based on selection
+            - list of values (dictionary with start and endtime and value)
+            - possible other values, such as min, max, average for hourly
+    """
+
+    if areas is None:
+        areas = []
+
+    if not isinstance(areas, list) and areas is not None:
+        areas = [i.strip() for i in areas.split(",")]
+
+    data_source = ("multiAreaEntries", "entryPerArea")
+
+
+    if data.get("status", 200) != 200 and "version" not in data:
+        raise Exception(f"Invalid response from Nordpool API: {data}")
+
+    currency = data.get("currency", currency)
+
+
+    start_time = None
+    end_time = None
+    # multiAreaDailyAggregates
+    if len(data[data_source[0]]) > 0:
+        start_time = _parse_dt(data[data_source[0]][0]["deliveryStart"])
+        end_time = _parse_dt(data[data_source[0]][-1]["deliveryEnd"])
+    updated = _parse_dt(data["updatedAt"])
+
+    area_data = {}
+
+    # Loop through response rows
+    for r in data[data_source[0]]:
+        row_start_time = _parse_dt(r["deliveryStart"])
+        row_end_time = _parse_dt(r["deliveryEnd"])
+
+        # Loop through columns
+        for area_key in r[data_source[1]].keys():
+            area_price = r[data_source[1]][area_key]
+            # If areas is defined and name isn't in areas, skip column
+            if area_key not in areas:
+                continue
+
+            # If name isn't in area_data, initialize dictionary
+            if area_key not in area_data:
+                area_data[area_key] = {
+                    "values": [],
+                }
+
+            # Append dictionary to value list
+            area_data[area_key]["values"].append(
+                {
+                    "start": row_start_time,
+                    "end": row_end_time,
+                    "value": _conv_to_float(area_price),
+                }
+            )
+
+    return {
+        "start": start_time,
+        "end": end_time,
+        "updated": updated,
+        "currency": currency,
+        "areas": area_data,
+    }
+
 async def join_result_for_correct_time(results, dt):
     """Parse a list of responses from the api
-    to extract the correct hours in there timezone.
+    to extract the correct hours in their timezone.
     """
     # utc = datetime.utcnow()
     fin = defaultdict(dict)
-    # _LOGGER.debug("join_result_for_correct_time %s", dt)
+    _LOGGER.debug("join_result_for_correct_time %s", dt)
     if dt is None:
         utc = datetime.now(ts.utc)
     else:
@@ -53,14 +146,8 @@ async def join_result_for_correct_time(results, dt):
             else:
                 zone = await dt_utils.async_get_time_zone(zone)
 
-            # We add junk here as the peak etc
-            # from the api is based on cet, not the
-            # hours in the we want so invalidate them
-            # its later corrected in the sensor.
-
             values = day_["areas"][key].pop("values")
 
-            # We need to check this so we dont overwrite stuff.
             if key not in fin["areas"]:
                 fin["areas"][key] = {}
             fin["areas"][key].update(value)
@@ -80,7 +167,7 @@ async def join_result_for_correct_time(results, dt):
                 if start_of_day <= local <= end_of_day:
                     if local == local_end:
                         _LOGGER.info(
-                            "Hour has the same start and end, most likly due to dst change %s exluded this hour",
+                            "Hour has the same start and end, most likely due to dst change %s excluded this hour",
                             val,
                         )
                     else:
